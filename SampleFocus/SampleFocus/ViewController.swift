e1ec20a2d6ed2d476a4bfd6f6149290b14872244
luisresende <luisfrvilela@gmail.com>
2016-05-09 14:35:46 -0300

    initial commit

---
+++ SampleFocus/SampleFocus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleFocus
+//
+//  Created by Luís Resende on 09/05/16.
+//  Copyright © 2016 Luis Resende. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
